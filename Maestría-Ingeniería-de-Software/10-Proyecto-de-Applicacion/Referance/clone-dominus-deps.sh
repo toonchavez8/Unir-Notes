b6fb74b866683f6c8b3b91dcdd6e3f7c1005f8d8
toonchavez8 <desarrollador5@sistemasicong.com>
2026-06-02 16:54:30 -0600

    vault backup: 2026-06-02 16:54:30

---
+++ Maestría-Ingeniería-de-Software/10-Proyecto-de-Applicacion/Referance/clone-dominus-deps.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+clone_or_update() {
+  local url="$1"
+  local name="$2"
+  local target="${SCRIPT_DIR}/${name}"
+
+  if [ -d "${target}/.git" ]; then
+    echo "Updating ${name}..."
+    git -C "${target}" pull --ff-only
+  else
+    echo "Cloning ${name}..."
+    git clone "${url}" "${target}"
+  fi
+}
+
+clone_or_update "https://github.com/unir-broker-tfm/dominus-broker.git" "dominus-broker"
+clone_or_update "https://github.com/unir-broker-tfm/dominus-sdk.git" "dominus-sdk"
+clone_or_update "https://github.com/unir-broker-tfm/dominus-proto-definition.git" "dominus-proto-definition"
+
+echo "Done."
